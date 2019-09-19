11b2b1a4eb234ad3076adeee976d59c542bd309b
Wanessa Daiana <wanessa_lima@estudante.sc.senai.br>
2019-09-19 16:55:49 -0300

    Sistema de geração de senha
    closes #12.

--- gerar_senha.php
+++ gerar_senha.php
@@ -1,3 +1,36 @@
+<?php
+require_once 'configDB.php';//Conexão com o Banco de Dados
+if(isset($_GET['token']) && strlen($_GET['token']) == 10){
+    $token = $_GET['token'];
+    $sql = $conecta->prepare("SELECT * FROM usuario WHERE token = ? AND tempo_de_vida> now()");
+    $sql->bind_param("s", $token);
+    $sql->execute();
+    $resultado = $sql->get_result();
+    if($resultado->num_rows > 0){
+        //echo "Nova senha:". @$_POST[senha];
+        //Salvar a nova senha no banco de dados
+        if(isset($_POST['senha'])){
+            $nova_senha = sha1($_POST['senha']);
+            $confirma_senha = sha1($_POST['csenha']);
+            if($nova_senha == $confirma_senha){
+                $sql = $conecta->prepare("UPDATE usuario SET senha = ?, token ='' WHERE token = ?");
+                $sql->bind_param("ss",$nova_senha,$token);
+                $sql->execute();
+                $msg = "Senha alterada com sucesso";
+            }else{
+                $msg = "As senhas não são iguais.";
+            }
+        }
+    }else{ //Token sem vida
+        header('location: index.php');
+        exit();
+    }
+}else{//Sem token ou token diferente de 10 caracteres
+    header('location:index.php');//Clique na página
+    exit();
+}
+?>
+
 <!doctype html>
 <html lang="pt-br">
 
--- recebe.php
+++ recebe.php
@@ -24,9 +24,16 @@ if(isset($_POST['action']) && $_POST['action'] == 'senha'){
         //Só para testar / debug
         //echo "<p class=\"text-success\">E-mail encontrado</p>";
         $frase = "Batatinhaquandonasce";
-        $frase_secreta = str_shuffle($frase);
-        $token = substr($frase_secreta, 0, 10);
-        echo "<p>$token</p>";
+        $frase_secreta = str_shuffle($frase);//Embaralha a frase
+        $token = substr($frase_secreta, 0, 10);//10 primeiros caracteres
+        //echo "<p>$token</p>";
+        $sql = $conecta->prepare("UPDATE usuario SET token = ?, tempo_de_vida = DATE_ADD(NOW(), INTERVAL 1 MINUTE) WHERE email = ?");
+        $sql->bind_param("ss", $token, $emailSenha);
+        $sql->execute();
+        //criação do Link para gerar nova senha
+        $link = "<a href=\"gerar_senha.php?token=$token\">Clique aqui para gerar uma nova senha</a>";
+        //Este link deve ser enviado por e-mail
+        echo $link;
     }else{
         echo '<p class="text-danger">E-mail não encontrado</p>';
     }
